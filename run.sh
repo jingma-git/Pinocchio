b7fd8763c87c155680bc225c4362ca458ca63c53
jingma-git <11921154@zju.edu.cn>
2022-06-14 11:14:52 +0800

    RigNet comparisons

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-./build/DemoUI data/chebu.off -motion data/walk.txt
+# ./build/DemoUI data/chebu.off -motion data/walk.txt
+# ./build/DemoUI /home/server/MaJing/Dataset/RigNet/data/obj/13.obj -motion data/walk.txt
+# ./build/DemoUI data/elephant.obj -motion data/walk.txt
+# ./build/DemoUI data/13w_low.obj -motion data/walk.txt
+./build/DemoUI data/13w_high.obj -motion data/walk.txt
