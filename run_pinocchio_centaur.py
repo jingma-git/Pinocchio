2367ce1eb0ee3ddfc1be23ca55f386fdbe3fde81
bellajingma <bellajingma@tencent.com>
2022-06-22 22:48:27 +0800

    for my own annotated dataset

--- run_pinocchio_centaur.py
+++ run_pinocchio_centaur.py
@@ -4,7 +4,6 @@ import numpy as np
 # data_dir = "/mnt/f/Dataset/RigNetv1/" # "F:/Dataset/RigNetv1/"
 data_dir = "/mnt/d/Dataset/AutoRS/"
 model_list = np.loadtxt(os.path.join(data_dir, "test_final.txt"), dtype=np.int32)
-# model_list = model_list[263:]
 # skip = [3685, 1333, 2921, 17736, 9466, 1236, 14836, 17872, 1226, 7665, 3724, 16552, 10497, 14455, 9853, 14519, 142]
 for model_id in model_list:
     if os.path.exists(os.path.join(data_dir, f"centaur/{model_id}.tgf")):
--- run_pinocchio_horse.py
+++ run_pinocchio_horse.py
@@ -4,7 +4,7 @@ import numpy as np
 # data_dir = "/mnt/f/Dataset/RigNetv1/" # "F:/Dataset/RigNetv1/"
 data_dir = "/mnt/d/Dataset/AutoRS/"
 model_list = np.loadtxt(os.path.join(data_dir, "test_final.txt"), dtype=np.int32)
-
+model_list = model_list[5:]
 for model_id in model_list:
     if os.path.exists(os.path.join(data_dir, f"horse/{model_id}.tgf")):
         continue
